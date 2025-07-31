f77c85f94810cf28489388ffa0fb910b01a43de2
Zaven477 <Papikyan86@bk.ru>
2025-07-31 13:57:37 +0300

    count images

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const API_KEY = "live_fHrUNHA96CMXDTbia9Xw4eSfJAovSZ0LWw5NPslMT79piyDM4IsdzPNJz5ktlzO3"
-export const COUNT_IMAGES = 10;
+export const COUNT_IMAGES = 15;
 export const FAVORITES_STORAGE_KEY = 'cat-gallery-favorites';
