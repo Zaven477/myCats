d80ba4db59b067d0fc04a0df5eda97d379ad05da
Zaven477 <Papikyan86@bk.ru>
2025-08-23 11:20:18 +0300

    add changes

--- src/features/CatsImages/store/reducer/types.ts
+++ src/features/CatsImages/store/reducer/types.ts
@@ -14,21 +14,8 @@ export type TStateCommentsSlice = {
   errors: string | null;
 };
 
-export type TUser = {
-  name: string;
-  login: string;
-  password: string;
-};
-
-export type TStateUserProfileSlice = {
-  users: null | TUser;
-  error: string | null;
-  loading: boolean;
-};
-
 export type RootState = {
   cats: EntityState<CurrentCats, string>;
   favoritesCats: EntityState<CurrentCats, string>;
   comments: TStateCommentsSlice;
-  users: TStateUserProfileSlice;
 };
