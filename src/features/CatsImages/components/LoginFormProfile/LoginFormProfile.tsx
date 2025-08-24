29fa024de0e6d9f3cfc692ed20bb81476d8916f5
Zaven477 <Papikyan86@bk.ru>
2025-08-24 12:33:31 +0300

    add changes

--- src/features/CatsImages/components/LoginFormProfile/LoginFormProfile.tsx
+++ src/features/CatsImages/components/LoginFormProfile/LoginFormProfile.tsx
@@ -1,6 +1,7 @@
 import { Formik } from "formik";
 import "./style.css";
 import { validationForm } from "./validation";
+import { ToastContainer } from "react-toastify";
 import { ClipLoader } from "react-spinners";
 import { useLoginFormProfile } from "./useLoginFormProfile";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
@@ -20,45 +21,48 @@ export const LoginFormProfile = () => {
         {(props) => {
           return (
             <form onSubmit={props.handleSubmit}>
-              <div className="wrapper-profile">
-                <div className="text-profile">Вход в личный кабинет</div>
-                <div>Логин:</div>
-                <input
-                  type="login"
-                  name="login"
-                  className={`input-login ${
-                    props.errors.login ? "borderLogin" : ""
-                  }`}
-                  onChange={props.handleChange}
-                />
-                <div className="error-login">{props.errors.login}</div>
-                <div>Пароль:</div>
-                <div className="wrapperInput">
+              <div className="container-shadow">
+                <div className="wrapper-profile">
+                  <div className="text-profile">Вход в личный кабинет</div>
+                  <div>Логин:</div>
                   <input
-                    type={isShowPassword ? "text" : "password"}
-                    name="password"
-                    className={`input-password ${
-                      props.errors.password ? "borderPassword" : ""
+                    type="login"
+                    name="login"
+                    className={`input-login ${
+                      props.errors.login ? "borderLogin" : ""
                     }`}
                     onChange={props.handleChange}
                   />
-                  <button
-                    type="button"
-                    className="btnEye"
-                    onClick={showPassword}
-                  >
-                    {isShowPassword ? <FaEye /> : <FaEyeSlash />}
+                  <div className="error-login">{props.errors.login}</div>
+                  <div>Пароль:</div>
+                  <div className="wrapperInput">
+                    <input
+                      type={isShowPassword ? "text" : "password"}
+                      name="password"
+                      className={`input-password ${
+                        props.errors.password ? "borderPassword" : ""
+                      }`}
+                      onChange={props.handleChange}
+                    />
+                    <button
+                      type="button"
+                      className="btnEye"
+                      onClick={showPassword}
+                    >
+                      {isShowPassword ? <FaEye /> : <FaEyeSlash />}
+                    </button>
+                  </div>
+                  <div className="error-password">{props.errors.password}</div>
+                  <button type="submit" className="btnSubmitProfile">
+                    {loading ? <ClipLoader size={25} color="blue" /> : "Войти"}
                   </button>
                 </div>
-                <div className="error-password">{props.errors.password}</div>
-                <button type="submit" className="btnSubmitProfile">
-                  {loading ? <ClipLoader size={25} color="blue" /> : "Войти"}
-                </button>
               </div>
             </form>
           );
         }}
       </Formik>
+      <ToastContainer className="location" />
     </div>
   );
 };
