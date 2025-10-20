8741e49f6a637f51d228692b519b872f167f89a1
Zaven477 <Papikyan86@bk.ru>
2025-10-20 11:20:13 +0300

    add changes

--- src/features/CatsImages/api/dataDashboard/getAttendanceUsers.ts
+++ src/features/CatsImages/api/dataDashboard/getAttendanceUsers.ts
@@ -1,9 +1,9 @@
 const data = [
   { date: "10 окт", users: 30 },
   { date: "11 окт", users: 80 },
-  { date: "12 окт", users: 130 },
-  { date: "13 окт", users: 100 },
-  { date: "14 окт", users: 120 },
+  { date: "12 окт", users: 50 },
+  { date: "13 окт", users: 160 },
+  { date: "14 окт", users: 40 },
   { date: "15 окт", users: 90 },
   { date: "16 окт", users: 340 },
 ];
